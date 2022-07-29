51322af4ac366f95f2653e06b480210340b8f48a
Meet Popat <78685308+404Meet@users.noreply.github.com>
2022-07-29 17:47:15 +0530

    2 new CS files added

---
+++ new.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Demo
+{
+    class new
+    {
+          static void Main(string args[])
+          {
+            int Salary =100000;
+            int Approx_Salary=100000;
+            const int Salary =100000;
+            const int Approx_Salary=100000;
+          }
+    }
+}
