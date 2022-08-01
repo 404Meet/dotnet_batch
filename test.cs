6112e210076cde2fe51e9233c05e027c16036bbc
Meet Popat <78685308+404Meet@users.noreply.github.com>
2022-08-01 16:18:05 +0530

    Commit by meet

---
+++ test.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            string message="Hello Meet";
+            System.Console.WriteLine( message);
+        }
+    }
+}
