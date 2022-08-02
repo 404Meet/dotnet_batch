af3749da887570ff1a440414696d85eefd6ef599
Meet Popat <78685308+404Meet@users.noreply.github.com>
2022-08-02 17:12:20 +0530

    DateDifference

---
+++ date.cs
@@ -0,0 +1,19 @@
+using System;
+namespace DifferenceTime{
+
+
+    class date{
+        public static void Main(string[] args){
+            DateTime d1=new DateTime(2018,6,23,5,46,34);
+            DateTime d2=new DateTime(2015,8,03,6,26,24);
+            DifferenceTime.Diff.diff(d1,d2);
+        }
+    }
+    class Diff{
+        public static void diff(DateTime t1, DateTime t2)
+        {
+             TimeSpan interval=t1-t2;
+            System.Console.WriteLine(interval);
+        }
+    }
+}
