1032ea87f425b2bc5f6fba67adbd587a64d510cf
Meet Popat <78685308+404Meet@users.noreply.github.com>
2022-08-03 14:31:21 +0530

    String and StringBuilder

---
+++ string.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+namespace StringUse{
+
+    public class String{
+        public static void str()
+        {
+        string first ="Hello";
+        string second = " My Name";
+        string third =" is Meet";
+        string New = string.Concat(first,second,third);
+        System.Console.WriteLine(New);
+        }
+
+    }
+    public class StringBd{
+        public static void strbd()
+        {
+            StringBuilder addword = new StringBuilder("HELLO ");
+            addword.Append(" Meet");
+            addword.Append(" Hey");
+            System.Console.WriteLine(addword);
+        }
+    }
+    public class Operate{
+         public static void Main(string[] args){
+            Operate op =new Operate();
+            StringUse.String.str();
+            StringUse.StringBd.strbd();
+    }
+}
+}
