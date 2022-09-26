e417c559b1866952278a49182f1d348ec1e92453
Meet Popat <78685308+404Meet@users.noreply.github.com>
2022-09-26 12:22:55 +0530

    Added Testing

---
+++ First_Unit_Test/First_Unit_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("First_Unit_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("First_Unit_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ff1c00b-524b-4d25-bbb1-8cb4f7c42b25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumExample/SeleniumExample/Program.cs
@@ -0,0 +1,57 @@
+﻿
+//using NUnit.Framework;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using System;
+ 
+//namespace Selenium_Automation
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            IWebDriver driver = new ChromeDriver("C:\\Users\\meet.popat\\Downloads\\chromedriver_win32");
+
+//            // This will open up the URL
+//            driver.Url = "https://www.geeksforgeeks.org/";
+//        }
+//    }
+//}
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\meet.popat\\Downloads\\chromedriver_win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
